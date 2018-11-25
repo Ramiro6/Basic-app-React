ac08b2a7c0ab114be82ecee08a93c4824ceff16e
ramiro6 <ramiroruiz578@gmail.com>
2018-11-25 10:05:56 +0100

    add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>ñasjdñlkaslñkdlkñask</h1>
+        <h1>ñasjdñlkaslñkdlkñask</h1>
       </div>
     );
   }
